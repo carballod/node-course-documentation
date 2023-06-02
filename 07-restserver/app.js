c9690a103d41d2393b850c939255e7d95f7f6e07
Daniela <danscarballo@gmail.com>
2023-06-02 19:50:34 -0300

    REST Server

---
+++ 07-restserver/app.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const Server = require("./MODELS/server");
+
+const server = new Server();
+
+server.listen();
---
+++ 07-restserver/controllers/user.js
@@ -0,0 +1,50 @@
+const { response } = require("express");
+
+const usuariosGet = (req, res = response) => {
+
+  const {q, nombre, page = "1", limit} = req.query;
+
+  res.json({
+    msg: "get API - controller",
+    q, nombre, page, limit
+  });
+};
+
+const usuariosPost = (req, res = response) => {
+  const { nombre, edad } = req.body;
+
+  res.status(201).json({
+    msg: "post API - controller",
+    nombre,
+    edad,
+  });
+};
+
+const usuariosPut = (req, res = response) => {
+  const id = req.params.id;
+
+  res.json({
+    msg: "put API - controller",
+    id,
+  });
+};
+
+const usuariosPatch = (req, res = response) => {
+  res.json({
+    msg: "patch API - controller",
+  });
+};
+
+const usuariosDelete = (req, res = response) => {
+  res.json({
+    msg: "delete API - controller",
+  });
+};
+
+module.exports = {
+  usuariosGet,
+  usuariosPost,
+  usuariosPut,
+  usuariosPatch,
+  usuariosDelete,
+};
